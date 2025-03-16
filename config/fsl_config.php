7e95704543d8910e8cb9e7450b03b96a92372577
nikirago <nick@yes-interactive.com>
2025-03-16 15:06:20 -0400

    updated default behavior to NOT echo. and added more echo options per request.

--- config/fsl_config.php
+++ config/fsl_config.php
@@ -9,7 +9,7 @@ function configure()
   option('base_uri', "/"); //set if app is not in web root directory but in a subdirectory...
   option('session', 'fsl'); // enable with a specific session name
   option('behind_proxy', FALSE);  //enabled if behind gateway or balancer like Kong
-  option('release', '20250313');  //enabled if behind gateway or balancer like Kong
+  option('release', '20250315');
   
   //##############################################
   //encryption configuration
